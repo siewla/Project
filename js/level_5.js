0918f24aa3020b2621d0532cb4bf03a2d41b1951
siewla <siewlapang@gmail.com>
2020-04-30 11:53:00 +0800

    change level 5 dbl click to single click

--- js/level_5.js
+++ js/level_5.js
@@ -8,10 +8,8 @@ const level5 = () => {
     $('.level-title').text(`Level 5 - Submit card ${getImgCardcode(cardArray[4])}`);
     let count = 0;
 
-
-
     for (let i=0; i<$('.level5-img').length; i++){
-        $('.level5-img').eq(i).on('dblclick',function (event){
+        $('.level5-img').eq(i).on('click',function (event){
             let imgid = $('.level5-img').eq(i)[0].id;
             if (imgid!==`index-${cardArray[4]}`){
                 $(this).css('display','none');
